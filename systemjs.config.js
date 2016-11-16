cc31d2e13d15064733b7179d2083a88bfdb74d9b
Simon Kristensen <simonsondrup@gmail.com>
2016-11-16 21:21:59 +0000

    Further in the angular quick-start tutorial

--- systemjs.config.js
+++ systemjs.config.js
@@ -24,6 +24,7 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      // 'moment': 'node_modules/moment/moment.js',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -37,7 +38,11 @@
       'angular-in-memory-web-api': {
         main: './index.js',
         defaultExtension: 'js'
-      }
+      },
+      // 'moment': {
+      //   main: './moment.js',
+      //   defaultExtension: 'js'
+      // }
     }
   });
 })(this);
